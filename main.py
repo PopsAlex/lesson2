ac8339affc9899534c9a9f051b9096a405d24624
PopsAlex <alexpops3000@gmail.com>
2024-08-03 18:53:04 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+homework_is_done = 12
+hours_spent = 1.5
+name_of_course = 'Python'
+time_for_one_task = hours_spent / homework_is_done
+print(f"Курс: {name_of_course}, "
+      f"всего задач: {homework_is_done}, "
+      f"затрачено часов: {hours_spent}, "
+      f"среднее время выполнения: {time_for_one_task} часа.")
